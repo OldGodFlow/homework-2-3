dca47f92e11145efaa9d35cb5ae9722568262963
Nikita <writebogdanov@gmail.com>
2023-09-17 23:05:35 +0300

    homework completed

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,17 @@
-public class Bicycle {
+public class Bicycle extends Vehicle {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
-    public String modelName;
-    public int wheelsCount;
+    public void changeTyre() {
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+                System.out.println("Меняем покрышку");
+            }
+    }
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    @Override
+    public void doService() {
+        System.out.println("Обслуживаем " + getModelName());
+        changeTyre();
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,17 @@
+public abstract class Vehicle implements Serviceable {
+    private final String modelName;
+    private final int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+}
